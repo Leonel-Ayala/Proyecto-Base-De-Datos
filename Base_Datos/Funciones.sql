a17daee7d59c535eb736d7213cd0de47c41fab5b
Leonel-Ayala <leonel.ayala2004@gmail.com>
2024-11-17 21:48:10 -0300

    Update Funciones.sql

--- Base_Datos/Funciones.sql
+++ Base_Datos/Funciones.sql
@@ -1,1 +1,20 @@
+BEGIN
+  LAROATLB(ARICA Y PARINACOTA);
+END;
 
+Arica y Parinacota
+Tarapacá
+Antofagasta
+Atacama
+Coquimbo
+Valparaíso
+Metropolitana de Santiago
+Libertador General Bernardo O'Higgins
+Maule
+Ñuble
+Biobío
+La Araucanía
+Los Ríos
+Los Lagos
+Aysén del General Carlos Ibáñez del Campo
+Magallanes y de la Antártica Chilena
